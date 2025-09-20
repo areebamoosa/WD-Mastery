1cc66176e6e6af7b5b0b1e342c5cb9d0a32af1d0
Areeba Moosa <areebamoosa153@gmail.com>
2025-09-21 01:58:05 +0500

    Completed Courses card component

--- src/components/Courses/Card.jsx
+++ src/components/Courses/Card.jsx
@@ -1,27 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import arrow from '../../assets/arrow.svg'
 
 const Card = ({ id, title, img, desc }) => {
   return (
     <>
-
       <Link to={`/course/${id}`} >
-
-        <div className=' rounded-2xl p-4 shadow-lg border border-yellow-500 hover:scale-105 transition flex flex-col gap-2 items-start justify-start w-90 h-80 card'>
-
-          <img src={img} alt="course-img" className='h-40 object-left object-contain card' />
-          <h2 className='text-white font-bold text-lg card'>{title}</h2>
-          <p className='light text-sm card'>{desc}...</p>
-
-          <div>
-            <p className='light-blue card'>Check it now </p>
-            {/* <p><i className="fa-solid fa-arrow-up"></i></p> */}
+        <div className="rounded-2xl p-4 border border-yellow-400 bg-[#0b0b2a] shadow-[0_0_20px_rgba(255,215,0,0.2)] hover:shadow-[0_0_25px_rgba(255,215,0,0.4)] hover:scale-[1.02] transition-all duration-300flex flex-col gap-2 items-start justify-start w-90 h-75 card">
+          <img src={img} alt="course-img" className='h-30 object-left object-contain card' />
+          <h2 className='text-white font-bold text-lg card mt-2'>{title}</h2>
+          <p className='light text-sm card mt-3'>{desc}...</p>
+          <div className='flex gap-2 card mt-6'>
+            <p className='light-blue card text-xs font-bold'>Check it now </p>
+            <img src={arrow} alt="arrow" className='w-3 card  ' />
           </div>
-
         </div>
-
       </Link>
-
     </>
   )
 }
--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import Card from './Card'
-import c1 from  '../../assets/c1.png'
-import c2 from  '../../assets/c2.png'
-
+import c1 from '../../assets/c1.png'
+import c2 from '../../assets/c2.png'
+import c3 from '../../assets/c3.png'
+import c4 from '../../assets/c4.png'
+import c5 from '../../assets/c5.png'
+import c6 from '../../assets/c6.png'
 
 const courses = [
-  {id: 1, title: "Web Animations Full Course", img: c1, desc:"Learn JSAP animation with WD Mastery."},
-  {id: 2, title: "Web Animations Full Course", img: c1, desc:"Learn JSAP animation with WD Mastery."},
-  {id: 3, title: "Web Animations Full Course", img: c1, desc:"Learn JSAP animation with WD Mastery."},
-  {id: 4, title: "Web Animations Full Course", img: c1, desc:"Learn JSAP animation with WD Mastery."},
-  {id: 5, title: "Web Animations Full Course", img: c1, desc:"Learn JSAP animation with WD Mastery."},
-  {id: 6, title: "Web Animations Full Course", img: c1, desc:"Learn JSAP animation with WD Mastery."},
+  { id: 1, title: "Web Animations Full Course", img: c1, desc: "Learn JSAP animation with WD Mastery.Practical tutorials, real projects with" },
+  { id: 2, title: "The Ultimate Next.js Course", img: c2, desc: "Learn Next.js from the ground up and build production-ready, fullstack React JS apps wit" },
+  { id: 3, title: "Vanilla Three.js Course", img: c3, desc: "JavaScript-based Web engine that can run GPU powered games and other graphics" },
+  { id: 4, title: "Build and launch your Saas in  7 Days", img: c4, desc: "A complete masterclass for quickly designing, developing, deploying and monetizing your  " },
+  { id: 5, title: "Database Mystery : SQL to Prisma", img: c5, desc: "Learn to build and use Databases with SQL and Prisma" },
+  { id: 6, title: "Complete Path to JavaScript Mastery", img: c6, desc: "Master JavaScript, the language of the web! Learn variable, datatypes, functions, DOM " },
 ];
 
 const Courses = () => {
   return (
     <>
-    <div className='grid grid-cols-3 gap-6 '>
-      {courses.map(course => (
-        <Card key={course.id} {...course}/>
-      ))}
-    </div>
+      <div className='grid grid-cols-3 gap-6 m-10 '>
+        {courses.map(course => (
+          <Card key={course.id} {...course} />
+        ))}
+      </div>
     </>
   )
 }
